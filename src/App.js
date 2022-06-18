191ca5e39568797243c68ebac8ca6df6e858430a
= <=>
2022-06-18 14:45:48 +0545

    Actually the second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Header from "./components/Header";
+import {Todos} from "./components/Todos";
+import {Todo} from "./components/Todo";
+import {Footer} from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title="My Todos List" />
+    <Todos/>
+    <Todo/>
+    <Footer/>
+    </>
   );
 }
 
