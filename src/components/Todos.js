41950063cca921dfb2af0481fdc1e2c11ae2f000
= <=>
2022-06-18 16:04:03 +0545

    Half of work done, learned lots of things

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Todo} from "../components/Todo";
 
-export const Todos = () => {
+export const Todos = (props) => {
+
   return (
-    <div>Todos works</div>
+    <div className='container'>
+        <h3 className='text-center my-3'>Todos List</h3>
+        {
+            props.todos.map((todo)=>{
+                return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+
+            })
+        }
+        
+    </div>
   )
 }
